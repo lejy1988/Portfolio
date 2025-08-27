b40f24abadc12043ab1697f6a827b8a75f4d3507
lejy1988 <lejy1988@gmail.com>
2025-08-27 13:23:05 +0100

    Portfolio
    Added in a couple more pages, started to work on links, added in a couple of pictures for the projects section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //this is for the typewriter effect
 document.addEventListener("DOMContentLoaded", function () {
-  const text = "Welcome to my first Portfolio page! You have no idea how long it took me to get this writing to be typed out one letter at a time...Hope you like it! 🎉";
+  const text = "Welcome to my first Portfolio page! I hope you enjoy it! 💻";
   const welcomeElement = document.getElementById("welcome-text");
   let index = 0;
 
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (index < text.length) {
       welcomeElement.innerHTML += text.charAt(index);
       index++;
-      setTimeout(typeWriter, 100); 
+      setTimeout(typeWriter, 50); 
     }
   }
 
